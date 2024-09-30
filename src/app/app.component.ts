e57c46d6bd3571c7922a466692e1714dcc8ab17d
Amarchikane <67099770+Amarchikane@users.noreply.github.com>
2024-09-30 22:01:43 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./Pages/components/header/header.component";
-import { FooterComponent } from "./Pages/components/footer/footer.component";
-import { CommonModule } from '@angular/common';
-
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, HeaderComponent, FooterComponent ,CommonModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'bismr';
-  iscontanerload: boolean = false;
-
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,3 @@
-
 import { Routes } from '@angular/router';
-import { HeaderComponent } from './Pages/components/header/header.component';
-import { FooterComponent } from './Pages/components/footer/footer.component';
-import { PageNotFoundComponent } from './Pages/components/page-not-found/page-not-found.component';
 
-export const routes: Routes = [
-  {
-    path:'header', loadChildren: () => import('./Pages/components/home/home-routing.module').then(c=> c.HomeRoutingModule)
-  },
-
-  {
-    path: 'footer',
-    loadChildren: () => import('./Pages/components/footer/footer-routing.module').then(c => c.FooterRoutingModule)
-  },
-  { path: '',
-    redirectTo: '/header',
-    pathMatch: 'full'
-  },
-  { path: '**', loadChildren: () => import('./Pages/modules/pagenotfound/PagenotfoundModule.module').then(c=> c.PagenotfoundModule) }
-
-];
+export const routes: Routes = [];
