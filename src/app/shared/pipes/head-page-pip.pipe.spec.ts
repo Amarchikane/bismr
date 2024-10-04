32c781f07189ad78850ff788cbaf8a15ff3a9888
Amarchikane <67099770+Amarchikane@users.noreply.github.com>
2024-10-04 16:31:25 +0530

    first commit

--- src/app/shared/pipes/head-page-pip.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HeadPagePipPipe } from './head-page-pip.pipe';
-
-describe('HeadPagePipPipe', () => {
-  it('create an instance', () => {
-    const pipe = new HeadPagePipPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
