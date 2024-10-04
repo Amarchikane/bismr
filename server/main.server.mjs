9e964a2100ed3eb938a809842ff97bb261f67bf9
Amarchikane <67099770+Amarchikane@users.noreply.github.com>
2024-10-04 16:33:32 +0530

    Auto-generated commit

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-F2GYWHNV.mjs";import"./chunk-HQPDC637.mjs";import"./chunk-4TL27K7E.mjs";import"./chunk-BGBOD5EE.mjs";import"./chunk-QPWC3QPD.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
