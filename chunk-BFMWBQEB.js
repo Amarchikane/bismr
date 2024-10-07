96a59f7c96c4821e33fdc1d0fadf4a4d07fafbce
Amarchikane <67099770+Amarchikane@users.noreply.github.com>
2024-10-07 15:09:34 +0530

    Auto-generated commit

---
+++ chunk-BFMWBQEB.js
@@ -0,0 +1,1 @@
+import{g as t}from"./chunk-HG5UIXDN.js";import"./chunk-TDGYOBRW.js";import{M as e,W as i}from"./chunk-266OR6RL.js";var m=[{path:"",loadComponent:()=>import("./chunk-YXIP3JRI.js").then(o=>o.HomeComponent)}],r=class o{static \u0275fac=function(n){return new(n||o)};static \u0275mod=i({type:o});static \u0275inj=e({imports:[t.forChild(m),t]})};export{r as HomeRoutingModule};
